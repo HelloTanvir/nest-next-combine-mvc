fc7ec10555d89d063fbb0be1749834824a415213
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-10-23 22:35:42 +0600

    frontend setup done

---
+++ src/client/pages/index.tsx
@@ -0,0 +1,5 @@
+import { NextPage } from 'next';
+
+const Home: NextPage = () => <h1>Hello from NextJS!</h1>;
+
+export default Home;
--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ViewModule } from './view/view.module';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true })],
+    imports: [ConfigModule.forRoot({ isGlobal: true }), ViewModule],
 })
 export class AppModule {}
