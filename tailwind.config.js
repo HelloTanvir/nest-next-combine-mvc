819a5798f4d5463dca5fa1b554335c59022c29b6
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-10-23 23:04:54 +0600

    added tailwind css on the project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/client/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/client/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
